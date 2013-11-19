54f5958d1af42df00924ff529f9a449d1734c4a2
Dan Bikle <dan@0xdata.com>
2013-11-19 00:22:33 +0000

    more

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ang10::Application.config.secret_key_base = 'd59eb2cc4f220fa2211a2ef280e64dfdcdfd8da2ceb5e93f1d605ad7e0903e0fa2b74a5016181ff2b570e2c549e1e02a752580f89346b4a24ae390681510c699'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Ang10::Application.config.session_store :cookie_store, key: '_ang10_session'
